34acd72ef4c17fef8714c7fbee0464737065020d
Chaitanya Chowdary <chaitanya.chowdary25@gmail.com>
2024-03-08 16:16:19 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Chaitanya Sudhakar',
+    author_email= 'chaitanya.chowdary25@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
